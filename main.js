6f716475e83fc453eb965f65025c1d7a4aa6165f
Renzo Alves <djenrolandy@gmail.com>
2022-11-03 19:52:00 -0300

    step_20221103 <Tornando animação dinamica>

--- main.js
+++ main.js
@@ -8,9 +8,13 @@ function createGame(player1, hour, player2) {
     `
 }
 
+let delay = -0.4;
+
 function createCard(date, day, games) {
+    delay += 0.4;
+
     return `
-    <div class="card">
+    <div class="card" style="animation-delay ${delay}s">
         <h2>${date}<span>${day}</span></h2>
         <ul>
             ${games}
