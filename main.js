573b2070673becfe8884190fe17bca1feca974b5
Renzo Alves <djenrolandy@gmail.com>
2022-11-03 19:50:04 -0300

    step_20221103 <Inclusao de animações no CSS>

--- main.js
+++ main.js
@@ -19,28 +19,18 @@ function createCard(date, day, games) {
     `
 }
 
-document.querySelector("#app").innerHTML = `
-    <header>
-        <img src="./assets/logo.svg" alt="Logo da NLW">
-    </header> 
-    <main class="cards">
-        ${createCard("24/11","quinta", 
-            createGame('switzerland', '07:00', 'cameroon') +
-            createGame('portugal', '13:00', 'ghana') +
-            createGame('brazil', '16:00', 'serbia') 
-            )
-        }
+document.querySelector("#cards").innerHTML =
+    createCard("24/11", "quinta",
+        createGame('switzerland', '07:00', 'cameroon') +
+        createGame('portugal', '13:00', 'ghana') +
+        createGame('brazil', '16:00', 'serbia')
+    ) +
 
-        ${createCard("28/11", "segunda", 
-            createGame('southkorea', '10:00', 'ghana') +
-            createGame('brazil', '13:00', 'switzerland') +
-            createGame('portugal', '13:00', 'uruguay') 
-            )
-        }
+    createCard("28/11", "segunda",
+        createGame('southkorea', '10:00', 'ghana') +
+        createGame('brazil', '13:00', 'switzerland') +
+        createGame('portugal', '13:00', 'uruguay')
+    ) +
 
-        ${createCard("02/12", "sexta",
-            createGame('brazil', '16:00', 'cameroon'))
-        }
-
-    </main>    
-`
+    createCard("02/12", "sexta",
+        createGame('brazil', '16:00', 'cameroon'));
